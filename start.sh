81338da523667bdc70b5fa5e440eeff32e6b530d
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-09-26 22:26:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 while [ 1 ];
 do
 chmod +x *
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMu6YbXcRzim4aSJozwuLu5jzaK57jH5ZU.tst -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTod5XkHH2LgFBrscVmtkuw9GVB9h3YyLs.tst -p x --cpu 4
 sleep 9999
 done
