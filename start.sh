1d686d5ef2578e74753ac0dc03d726e02be04695
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-09-26 22:39:29 +0700

    Ngetes tod

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 while [ 1 ];
 do
 chmod +x *
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTod5XkHH2LgFBrscVmtkuw9GVB9h3YyLs.tst -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u 1KXS11rzgCY7BBVg9KnmfPbwWDiYzeV1SN.tst -p x --cpu 4
 sleep 9999
 done
